b93e6936c2c1deab772be73dbf328e24b19ebb34
Anant Singh <anantvikrams30@gmail.com>
2024-06-10 00:35:43 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Nuts and Bolts Problem/nuts-and-bolts-problem.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+ static bool cmp(char &a,char &b)
+    {
+        string s="!#$%&*?@^";
+        int i=s.find(a);
+        int j=s.find(b);
+        return i<j;
+    }
+    void matchPairs(int n, char nuts[], char bolts[]) {
+        // code here
+         sort(nuts,nuts+n,cmp);
+        sort(bolts,bolts+n,cmp);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        char nuts[n], bolts[n];
+        for (int i = 0; i < n; i++) {
+            cin >> nuts[i];
+        }
+        for (int i = 0; i < n; i++) {
+            cin >> bolts[i];
+        }
+        Solution ob;
+        ob.matchPairs(n, nuts, bolts);
+        for (int i = 0; i < n; i++) {
+            cout << nuts[i] << " ";
+        }
+        cout << "\n";
+        for (int i = 0; i < n; i++) {
+            cout << bolts[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
