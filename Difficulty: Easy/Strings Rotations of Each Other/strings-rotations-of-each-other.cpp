34775f6779a9b45cad3fb1a54d722fba601b8b15
Anant Vikram Singh <anantvikrams30@gmail.com>
2024-12-04 15:36:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Strings Rotations of Each Other/strings-rotations-of-each-other.cpp
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+      vector<int> buildLPS(const string& pattern) {
+    int n = pattern.size();
+    vector<int> lps(n, 0);
+    int len = 0; 
+    int i = 1;
+    while (i < n) {
+        if (pattern[i] == pattern[len]) {
+            len++;
+            lps[i] = len;
+            i++;
+        } else {
+            if (len != 0) {
+                len = lps[len - 1];
+            } else {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+    return lps;
+}
+    // Function to check if two strings are rotations of each other or not.
+    bool areRotations(string &text, string &pattern) {
+        // Your code here
+        text=text+"$"+text+text;
+            int m = pattern.size();
+    int n = text.size();
+    vector<int> lps = buildLPS(pattern);
+    int i = 0,j = 0;
+    while (i < n) {
+        if (text[i] == pattern[j]) {
+            i++;
+            j++;
+        }
+        if (j == m) {
+            return true;
+        } else if (i < n && text[i] != pattern[j]) {
+            if (j != 0) {
+                j = lps[j - 1];
+            } else {
+                i++;
+            }
+        }
+    }
+    return false; 
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s1;
+        string s2;
+        cin >> s1 >> s2;
+        Solution sol;
+        int result = sol.areRotations(s1, s2);
+        if (result) {
+            cout << "true" << endl;
+
+        } else {
+            cout << "false" << endl;
+        }
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
