184caa1733815f6785a4cb531a0e9e52d5097ff0
Anant Vikram Singh <anantvikrams30@gmail.com>
2025-04-17 13:55:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Floyd Warshall/floyd-warshall.cpp
+++ Difficulty: Medium/Floyd Warshall/floyd-warshall.cpp
@@ -16,7 +16,7 @@ class Solution {
          int n = dist.size();
         for (int k = 0; k < n; k++) {
             for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
+                for (int j = 0; j<n; j++) {
                     if (dist[i][k] < 1e8 && dist[k][j] < 1e8) {
                         dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
                     }
