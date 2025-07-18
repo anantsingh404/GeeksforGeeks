2daf192ece04ce8ec9b1c9b336bc85a824949563
Anant Vikram Singh <anantvikrams30@gmail.com>
2025-07-18 22:14:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/LCM Triplet/lcm-triplet.cpp
+++ Difficulty: Medium/LCM Triplet/lcm-triplet.cpp
@@ -1,6 +1,8 @@
 class Solution {
   public:
-   int gcd(int a, int b){
+   int gcd(int a, int b)
+   
+   {
         if(b == 0) return a;
         return gcd(b, a % b);
     }
@@ -9,7 +11,8 @@ class Solution {
         return (a/gcd(a, b) * b);
     }
   
-    int lcmTriplets(int n) {
+    int lcmTriplets(int n) 
+    {
         if(n < 3) return n;
         
         if(n & 1) return lcm(n, lcm(n-1, n-2));
