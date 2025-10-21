0a70c54a44aee633f3173c41f5429af08dbb0edc
Anant Vikram Singh <anantvikrams30@gmail.com>
2025-10-22 00:20:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Nearly sorted/nearly-sorted.cpp
+++ Difficulty: Medium/Nearly sorted/nearly-sorted.cpp
@@ -1,66 +1,22 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function Template for C++
 class Solution {
   public:
     void nearlySorted(vector<int>& arr, int k) {
-        // code
-       vector<int>anss;
-       int num=arr.size();
-        priority_queue<int,vector<int>,greater<int>>ans;
-        int j=0;
-        
-        for(int i=0;i<num;i++)
+        // code here
+        priority_queue<int,vector<int>,greater<int>>st;
+        int i=0;int j=0;
+        while(j<arr.size())
         {
-            if(ans.size()==k+1)
-            {
-                arr[j]=ans.top();
-                j++;
-                ans.pop();
+            st.push(arr[j]);
+            if(st.size()==k+1){
+                arr[i++]=st.top();
+                st.pop();
             }
-            ans.push(arr[i]);
-        }
-        while(!ans.empty())
-        {
-            arr[j]=ans.top();
-            ans.pop();
             j++;
         }
-        
-        
+        while(!st.empty())
+        {
+            arr[i++]=st.top();
+            st.pop();
+        }
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        string ks;
-        getline(cin, ks);
-        int k = stoi(ks);
-        Solution obj;
-        obj.nearlySorted(arr, k);
-        for (int i = 0; i < arr.size(); i++) {
-            cout << arr[i] << " ";
-        }
-        cout << endl;
-        // cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
