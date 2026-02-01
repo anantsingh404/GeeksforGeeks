ca86addfa4850ee8004d6e23c7ec6f6f8259f01d
Anant Vikram Singh <anantvikrams30@gmail.com>
2026-02-02 00:18:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Max Circular Subarray Sum/max-circular-subarray-sum.cpp
+++ Difficulty: Hard/Max Circular Subarray Sum/max-circular-subarray-sum.cpp
@@ -2,41 +2,44 @@ class Solution {
   public:
     int maxCircularSum(vector<int> &arr) {
         // code here
-        
+            int sum=0;
         int n=arr.size();
         int ans=INT_MIN;
-        int sum=0;
-        vector<int>left(n,0);
-        vector<int>right(n,0);
-        int t=arr[0];
+        vector<int>left(n,INT_MIN);
+        vector<int>right(n,INT_MIN);
         left[0]=arr[0];
+        sum=arr[0];
+        right[n-1]=arr[n-1];
+        int maxi=arr[0];
         for(int i=1;i<n;i++)
         {
-            t+=arr[i];
-            left[i]=max(left[i-1],t);
+            sum+=arr[i];
+            maxi=max(maxi,sum);
+            left[i]=maxi;
         }
-        t=arr[n-1];
-        right[n-1]=arr[n-1];
+        sum=arr[n-1];
+        maxi=arr[n-1];
         for(int i=n-2;i>=0;i--)
         {
-            t+=arr[i];
-            right[i]=max(right[i+1],t);
-        }
-        int i=0;
-        while(i<n)
-        {
             sum+=arr[i];
-            ans=max(ans,sum);
-            if(sum<0)
-            {
-                sum=0;
-            }
-            i++;
+            maxi=max(maxi,sum);
+            right[i]=maxi;
         }
-        for(int i=0;i<n-1;i++)
+    for(int i=0;i<n-1;i++)
+    {   
+        ans=max(ans,left[i]+right[i+1]);
+    }
+    sum=0;
+    for(int i=0;i<n;i++)
+    {
+        sum+=arr[i];
+        
+        ans=max(ans,sum);
+        if(sum<0)
         {
-            ans=max(ans,left[i]+right[i+1]);
+            sum=0;
         }
-        return ans;
+    }
+    return ans;
     }
 };
